a61db16d8049d2c1d0d0b31c22cd17032984a56e
Matthias Nilsson <112242026+RalfiSlask@users.noreply.github.com>
2023-09-29 09:09:52 +0200

    29th sep refactoring

--- index.js
+++ index.js
@@ -35,12 +35,8 @@ const heightOffCharts = (chartBodies, jsonData) => {
 const highlightCurrentDay = (currentDay, chartBodies) => {
     const date = Date();
     const dateArray = date.split(" ");
-    let weekday;
-
-    dateArray.forEach(date => {
-        weekday = date.toLowerCase();
-    });
-
+    let weekday = dateArray[0].toLowerCase();
+   
     currentDay.forEach((day, index) => {
         const chartBody = chartBodies[index]
         if(weekday === day.innerHTML) {
@@ -63,8 +59,8 @@ const fetchJsonData = async ()  => {
         let currentDay = document.querySelectorAll(".day");
 
         heightOffCharts(chartBodies, jsonData)
-        highlightCurrentDay(currentDay, chartBodies);
         showAmountOnHover(chartBodies, dailyAmounts);
+        highlightCurrentDay(currentDay, chartBodies);
 
         } else {
             throw new Error("response not okey")
@@ -75,63 +71,4 @@ const fetchJsonData = async ()  => {
     }
 };
 
-/* fetchJsonData(); */
-
-fetch("./data.json")
-.then(response => {
-    return response.json();
-})
-.then(json => {
-    for(let i = 0; i < json.length; i++) {
-    let amount = json[i].amount;
-    let day = json[i].day;
-    let chart = document.createElement("div");
-    chart.className = "chart";
-    chart.innerHTML = 
-    `<div class="amount hidden">$${json[i].amount}</div>
-    <div class="body monday"></div>
-    <div class="day">${json[i].day}</div>`
-    charts.appendChild(chart);
-    }
-
-    let dailyAmounts = document.querySelectorAll(".amount");
-    let chartBodies = document.querySelectorAll(".body");
-    let currentDay = document.querySelectorAll(".day");
-
-    const showAmountOnHover = () => {
-        for(let i = 0; i < chartBodies.length; i++) {
-            chartBodies[i].onmouseenter = () => {
-            dailyAmounts[i].classList.remove("hidden");
-         }
-            chartBodies[i].onmouseleave = () => {
-            dailyAmounts[i].classList.add("hidden");
-         }
-        }
-    }
-
-    const heightOffCharts = () => {
-        for(let i = 0; i < chartBodies.length; i++) {
-            chartBodies[i].style.height = `${json[i].amount * 3}px`;
-        }
-    }
-
-    const highlightCurrentDay = () => {
-        const date = Date();
-        const dateArray = date.split(" ");
-        let weekday;
-        for(let i = 0; i < dateArray.length; i++) {
-            weekday = dateArray[0].toLowerCase();
-        }
-        for(let i = 0; i < currentDay.length; i++) {
-            if(weekday == currentDay[i].innerHTML) {
-               chartBodies[i].classList.toggle("currentDay"); 
-            }
-        }
-    } 
-
-    highlightCurrentDay();
-    showAmountOnHover();
-    heightOffCharts();
-});
-
-
+fetchJsonData(); 
